d9f428a5077415efbf7d8bd569aeb906b31d218f
Bill Zerega <zerega85@gmail.com>
2016-03-01 17:18:08 -0500

    got post create and show working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,38 @@
 class PostsController < ApplicationController
+
+  def get_post
+    Post.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def index
+    @post = Post.new
+    @posts = Post.all
+
+  end
+  def create
+    puts params
+    @post = Post.new(params[:post])
+    if @post.save
+      flash[:notice] = "Post was successful!"
+      redirect_to '/'
+    else
+      flash[:alert] = "There was an error creating your post :("
+        redirect_to '/'
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
 end
