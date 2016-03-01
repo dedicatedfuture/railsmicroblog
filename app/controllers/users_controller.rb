63f7456b923b54271a0147be071f6330140a6a0b
Nancy Blayney <nblayney@g.cofc.edu>
2016-03-01 16:29:42 -0500

    created create action for the user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
 	def index
 	end
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		puts "****************************************************************************************************************"
+		puts params
+		@user = User.new(params[:user])
+		if @user.save 
+			flash[:notice] = "User was successfully created"
+			redirect_to '/'
+		else
+			flash[:alert] = "There was an error creating your user"
+			redirect_to '/users/new'
+		end
+	end
+
 
 end
