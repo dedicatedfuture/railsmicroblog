d33c8cf13112feec5ed99447264bd3c2b9559d3b
Nancy Blayney <nblayney@g.cofc.edu>
2016-03-02 09:48:09 -0500

    added an edit page for the posts, the update action is working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,10 +26,22 @@ class CommentsController < ApplicationController
   end
 
   def update
-  	
+  	@comment = get_post
+    @comment.update(params[:comment])
+    if @comment.save
+      flash[:notice] = "Your comment was successfully updated"
+      redirect_to '/'
+    else
+      flash[:alert] = "There was an error changing your comment."
+      redirect_to '/'
+    end
   end
 
   def destroy
+  	@comment = get_comment
+    Comment.destroy(@comment)
+    flash[:notice] = "Your comment has been deleted!"
+    redirect_to '/'
   end
 
 
