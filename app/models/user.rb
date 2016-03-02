17c5708cbbd636887421cc4a7f00f37e622ddc52
Bill Zerega <zerega85@gmail.com>
2016-03-02 14:21:18 -0500

    added delete posts/comments to delete user action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :comments
+  before_destroy :destroy_posts_comments
+
+  def destroy_posts_comments
+    self.posts.destroy_all
+    self.comments.destroy_all
+  end
 end
