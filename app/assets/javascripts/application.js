d7404532259b53da01692f741fbd3eb3c630c55e
Nancy Blayney <nblayney@g.cofc.edu>
2016-03-04 15:40:20 -0500

    FIXED ISSUE WITH COMMENTS FINALLY - ISSUE WAS WITH TURBOLINKS - IT WAS INTERFERING WITH JQUERY - JQUERY WASNT FIRING BECAUSE OF ISSUES WITH THE PAGE REFRESH DUE TO TURBOLINKS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,22 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function(){
+	
+	$(".comment-button").click(function(e){
+		e.preventDefault();
+		var $thisPost = $(this).closest('.whole-post');
+		$thisPost.find(".comment-field").toggle();
+	})
+
+	$(".comment-view").click(function(e){
+		e.preventDefault();
+		var $thisPost = $(this).closest('.whole-post');
+		$thisPost.find(".comment").toggle();
+	})
+
+
+});
